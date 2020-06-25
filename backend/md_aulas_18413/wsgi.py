a533f9eed4dd12ea8105dda1f5a4ec4910bf0831
Crowdbotics <team@crowdbotics.com>
2020-06-25 06:34:40 +0000

    Update models

--- backend/md_aulas_18413/wsgi.py
+++ backend/md_aulas_18413/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'md_aulas_18413.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "md_aulas_18413.settings")
 
 application = get_wsgi_application()
